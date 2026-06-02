11e32c37d33625f592f6616f2eb9b410559ee216
Gioia1909 <g.iannuzzi19@studenti.unisa.it>
2026-06-02 09:32:30 +0200

    aggiunta del CSS per la barra e cambio delle icone

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,8 @@ module com.example.gruppo04 {
     requires org.kordamp.bootstrapfx.core;
     requires java.desktop;
     requires mp3agic;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.materialdesign2;
     opens com.example.gruppo04 to javafx.fxml;
     // Allow JavaFX modules to reflectively access model classes (PropertyValueFactory)
     opens com.example.gruppo04.model to javafx.base, javafx.fxml;
