35d6bf533c7e3d4dcecd16f5085c6e12857566a5
Annamaria Scermino <a.scermino7@studenti.unisa.it>
2026-06-06 23:15:16 +0200

    gestione del caricamento e del salvataggio ad ogni modifica sul catalogo

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -27,4 +27,6 @@ module com.example.gruppo04 {
     opens com.example.gruppo04.view to javafx.fxml;
     exports com.example.gruppo04.model.state;
     opens com.example.gruppo04.model.state to javafx.base, javafx.fxml;
+    exports com.example.gruppo04.persistence;
+    opens com.example.gruppo04.persistence to javafx.fxml;
 }
