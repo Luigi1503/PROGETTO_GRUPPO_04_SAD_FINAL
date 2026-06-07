2192ef0384e8eb673e708229c76424ec6dc01226
Annamaria Scermino <a.scermino7@studenti.unisa.it>
2026-06-07 08:41:32 +0200

    Salvataggio delle modifiche effettuate sul catalogo

--- src/main/java/com/example/gruppo04/MainApplication.java
+++ src/main/java/com/example/gruppo04/MainApplication.java
@@ -30,28 +30,29 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
+        // ── Registra Observer per salvataggio automatico ──
+        AutoSaveObserver autoSave = new AutoSaveObserver(catalog);
+        catalog.registerObserver(autoSave);
+
         // ── Caricamento stato persistente ─────────
         PersistenceManager pm = PersistenceManager.getInstance();
         try {
             pm.load();
+            logger.log(Level.INFO, "Stato del catalogo caricato con successo.");
         } catch (IOException | ClassNotFoundException e) {
-            logger.log(Level.INFO, "Nessun salvataggio trovato, avvio con catalogo vuoto");
+            // file non esiste — prima volta che si avvia l'app
+            logger.log(Level.INFO, "Nessun salvataggio trovato, avvio con catalogo di esempio.");
+
+            // ── Libreria iniziale di esempio ──────────
+            catalog.addTrack(new TrackImpl("Hold Back The River", "James Bay",    "Rock",      2014, 354, null));
+            catalog.addTrack(new TrackImpl("Someday",             "OneRepublic",  "Pop",       2021, 391, null));
+            catalog.addTrack(new TrackImpl("As It Was",           "Harry Styles", "Pop",       2022, 300, null));
+            catalog.addTrack(new TrackImpl("Levitating",          "Dua Lipa",     "Pop",       2020, 203, null));
+            catalog.addTrack(new TrackImpl("Blinding Lights",     "The Weeknd",   "Synth-Pop", 2019, 200, null));
+
+            catalog.createPlaylist("Rock Classics");
+            catalog.createPlaylist("Late Night Vibes");
         }
-
-        // ── Libreria iniziale di esempio ──────────
-        catalog.addTrack(new TrackImpl("Hold Back The River", "James Bay",     "Rock",      2014, 354, null));
-        catalog.addTrack(new TrackImpl("Someday",  "OneRepublic",    "Pop",      2021, 391, null));
-        catalog.addTrack(new TrackImpl("As It Was", "Harry Styles", "Pop", 2022, 300, null));
-        catalog.addTrack(new TrackImpl("Levitating", "Dua Lipa", "Pop", 2020, 203, null));
-        catalog.addTrack(new TrackImpl("Blinding Lights", "The Weeknd", "Synth-Pop", 2019, 200, null));
-
-        // ── Crea playlist di esempio ──────────────
-        catalog.createPlaylist("Rock Classics");
-        catalog.createPlaylist("Late Night Vibes");
-
-        // ── Registra Observer per salvataggio automatico ──
-        AutoSaveObserver autoSave = new AutoSaveObserver(catalog);
-        catalog.registerObserver(autoSave);
 
 
         // ── Crea i controller ─────────────────────
