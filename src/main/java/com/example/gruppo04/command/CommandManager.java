e6cd71fdf74a5a52700e6ae46de999c679618721
Luigi1503 <montonettiluigi@icloud.com>
2026-06-13 12:54:03 +0200

    commandManager

---
+++ src/main/java/com/example/gruppo04/command/CommandManager.java
@@ -0,0 +1,47 @@
+package com.example.gruppo04.command;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+
+import java.util.Stack;
+
+
+public class CommandManager {
+    private static final CommandManager instance = new CommandManager();
+    private Stack<Command> stack;
+    private final BooleanProperty canUndoProperty = new SimpleBooleanProperty(false);
+
+    private CommandManager() {
+        this.stack = new Stack<>();
+    }
+
+    public static CommandManager getInstance() {
+        return instance;
+    }
+
+    public void executeCommand(Command cmd){
+        if (cmd != null) {
+            cmd.execute();
+            stack.push(cmd);
+            updateUndoState(); // Aggiorna il valore del megafono
+        }
+    }
+
+    public void undo(){
+        if(!stack.isEmpty()){
+            Command command = stack.pop();
+            command.undo();
+            updateUndoState(); // Aggiorna il valore del megafono
+        }
+    }
+
+    private void updateUndoState() {
+        // canUndoProperty.set() "spara" la notifica a tutti i listener in ascolto
+        canUndoProperty.set(!stack.isEmpty());
+    }
+
+    public BooleanProperty canUndoProperty() {
+        return canUndoProperty;
+    }
+
+}
